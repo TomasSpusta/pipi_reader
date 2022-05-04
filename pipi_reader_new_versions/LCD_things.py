d334dfbb0f67d19f1a876764ae421834e05e674d
ImpaledTom <pokusik1@gmail.com>
2022-05-04 12:19:19 +0200

    k

--- pipi_reader_new_versions/LCD_things.py
+++ pipi_reader_new_versions/LCD_things.py
@@ -7,8 +7,8 @@ def LCD_waiting (instrument_name):
     #global logged_in
     #logged_in = False
     lcd.clear() #clear the display
-    lcd.text("Welcome on" , 1)  #print/show string on line 2
-    lcd.text(instrument_name, 2) #print/show string on line 3
+    lcd.text("Welcome on " + instrument_name, 1)  #print/show string on line 2
+    #lcd.text(instrument_name, 2) #print/show string on line 3
     lcd.text("Please log in with your user card",3)
     
 def LCD_init ():
