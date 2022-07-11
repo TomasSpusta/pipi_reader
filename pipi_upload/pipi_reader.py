3ac9e4d060d5e22c3e9709d9c306104cf2d54281
ImpaledTom <pokusik1@gmail.com>
2022-07-11 16:24:17 +0200

    fasdf

--- pipi_upload/pipi_reader.py
+++ pipi_upload/pipi_reader.py
@@ -55,7 +55,8 @@ def main_script():
                     LCD_display.about_to_end_w()
                 if config.in_session == True and config.status_code == 404:
                     LCD_display.session_ended () 
-                    config.in_session = False       
+                    config.in_session = False
+                    config.remaining_time = 0       
                     
             LCD_display.session_ended ()  
             config.in_session = False
