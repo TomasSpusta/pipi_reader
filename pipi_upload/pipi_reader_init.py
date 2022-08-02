a3360fc3025078d4a8dd71ea350767b28a051403
ImpaledTom <pokusik1@gmail.com>
2022-08-02 13:41:10 +0200

    fasdfa

--- pipi_upload/pipi_reader_init.py
+++ pipi_upload/pipi_reader_init.py
@@ -12,46 +12,54 @@ from getmac import get_mac_address as gma #module for mac adress
 import time
 import LCD_display
 import web_requests
+import RPi.GPIO as GPIO
 
 pi_online_status = False
 # try to acquire IP adress, therefore check connection to the internet
-while pi_online_status == False:
-    try:
-        ip = get('https://api.ipify.org').content.decode('utf8')
-        print('My public IP address is: {}'.format(ip))    
-        pi_online_status = True
+try:
         
-    except Exception as e: # if there is an error = no connection to net, ip = 0
-        ip = 0
-        print (e) 
-     
-    try:   
-        config.mac_address = gma() # get MAC address
-        print("My MAC adress is: {}".format(config.mac_address))
-        
+    while pi_online_status == False:
         try:
-            web_requests.crm_request_mac()
-        except Exception as  e2:
-            print (e2)
-            print ("problemek s CRM")
-    except Exception as e3:
-        config.mac_address = 0
-        print (e3)
+            ip = get('https://api.ipify.org').content.decode('utf8')
+            print('My public IP address is: {}'.format(ip))    
+            pi_online_status = True
+            
+        except Exception as e: # if there is an error = no connection to net, ip = 0
+            ip = 0
+            print (e) 
+        
+        try:   
+            config.mac_address = gma() # get MAC address
+            print("My MAC adress is: {}".format(config.mac_address))
+            
+            try:
+                web_requests.crm_request_mac()
+            except Exception as  e2:
+                print (e2)
+                print ("problemek s CRM")
+        except Exception as e3:
+            config.mac_address = 0
+            print (e3)
 
-print (config.equipment_name, config.equipment_id)        
+    print (config.equipment_name, config.equipment_id)        
 
-LCD_display.LCD_init (ip, config.mac_address)
-time.sleep (5)
-
-
+    LCD_display.LCD_init (ip, config.mac_address)
+    time.sleep (5)
 
 
-if ip != 0:
-    time.sleep (1)    
-    print ("Checking github")
-    import github_repo
-    
-    time.sleep (1) #Sleep x seconds before it will run main script 
-    print ("Main script start")
-    import pipi_reader
 
+
+    if ip != 0:
+        time.sleep (1)    
+        print ("Checking github")
+        import github_repo
+        
+        time.sleep (1) #Sleep x seconds before it will run main script 
+        print ("Main script start")
+        import pipi_reader
+
+finally:
+    time.sleep(0.5)
+    LCD_display.backlight (False)
+    LCD_display.clear ()        
+    GPIO.cleanup()
