fe34241f225cc60fd5e524796710781c782054ac
ImpaledTom <pokusik1@gmail.com>
2022-09-01 17:43:38 +0200

    dsa

--- pipi_upload/pipi_reader.py
+++ pipi_upload/pipi_reader.py
@@ -50,7 +50,7 @@ def main_script():
             print("Recording ID: " + str(config.recording_id))
             print("Reservation ID: " + str(config.reservation_id))
            
-            logs.start()
+            #logs.start()
             
             refresh_rate = 10 #refresh rate of remaining time and files in seconds    
             while config.remaining_time > 0 :
