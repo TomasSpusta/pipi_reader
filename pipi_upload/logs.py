433c7daabc4d3520a302b56850f67d65cb95292f
ImpaledTom <pokusik1@gmail.com>
2022-09-02 11:31:41 +0200

    added end()

--- pipi_upload/logs.py
+++ pipi_upload/logs.py
@@ -30,9 +30,23 @@ def start():
     
    
 def end():
-    logging.info ( "end",
-    
-        config.mac_address, config.card_id
-    )
+    logging.info ((("\n Instrument info:     \
+                    \n MAC Address: {}     \
+                    \n Equipment ID: {}     \
+                    \n Equipment alias: {}  \
+                    \n                      \
+                    \n User info:           \
+                    \n User ID: {}          \
+                    \n Card ID: {}          \
+                    \n User name: {}        \
+                    \n                      \
+                    \n Reservation info:     \
+                    \n Reservation ID: {}   \
+                    \n Recording ID: {}") 
+                   .format( 
+                    config.mac_address, config.equipment_id, config.equipment_name,
+                    config.user_id, config.card_id, config.user_full_name,
+                    config.reservation_id, config.recording_id
+                    )))
     
  
