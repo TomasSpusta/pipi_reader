64ff55ab508aca3596381607246687031cd6820e
ImpaledTom <pokusik1@gmail.com>
2022-12-02 10:32:05 +0100

    dfs

--- pipi_upload/card_reader.py
+++ pipi_upload/card_reader.py
@@ -8,5 +8,18 @@ reader = SimpleMFRC522()
 #Parameter rfid is card number from MFRC522 reader
 
 def card_reader():
-    config.card_id, text = reader.read()
-    #print ('Readed card: ' + str(rfid))
+    RFID_id, text = reader.read()
+    #print ('Readed card: ' + str(rfid)) 
+    
+    # convert decimal number from RFID reader to hexadecimal number
+    hex_num = hex (RFID_id)
+    # trim the last 2 characters from the hexadecimal
+    trimmed_hex_num = hex_num [2:10:]
+    print (trimmed_hex_num)
+    
+    altered_hex_num = trimmed_hex_num [6] + trimmed_hex_num [7] + trimmed_hex_num [4] + trimmed_hex_num [5] + trimmed_hex_num [2] + trimmed_hex_num [3] + trimmed_hex_num [0] + trimmed_hex_num [1]
+    
+    print (altered_hex_num)
+    
+    new_dec_num = int (altered_hex_num,16)
+    print (new_dec_num)
