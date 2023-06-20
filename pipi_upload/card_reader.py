1901e7c7ecff2041126423142292f90f28550562
ImpaledTom <pokusik1@gmail.com>
2023-06-20 10:31:46 +0200

    several updates

--- pipi_upload/card_reader.py
+++ pipi_upload/card_reader.py
@@ -15,14 +15,10 @@ def card_reader():
     hex_num = hex (RFID_id)
     # trim the last 2 characters from the hexadecimal number
     trimmed_hex_num = hex_num [2:10:]
-    
-    #print (trimmed_hex_num)
-    
+       
     # translate the entities in hex format to the hex format from PC RFID reader
     altered_hex_num = trimmed_hex_num [6] + trimmed_hex_num [7] + trimmed_hex_num [4] + trimmed_hex_num [5] + trimmed_hex_num [2] + trimmed_hex_num [3] + trimmed_hex_num [0] + trimmed_hex_num [1]
-    
-    #print (altered_hex_num)
-    
+       
     # convert altered hexadecimal number to the new decimal number, which will be the card_id sent to the API
     converted_altered_hex_num = str (int (altered_hex_num,16))
     #print (converted_altered_hex_num)
@@ -34,5 +30,3 @@ def card_reader():
     
     #print (config.card_id)
     #print (len (config.card_id))
-    
-    # TO DO: there is prbolem with cards starting with 0 ZERO, there needs to be condition, when lenght card_id is 9, add zero on the firrts place.
