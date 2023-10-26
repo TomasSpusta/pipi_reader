29553db7082b11e0d19330e44177a58b710b028c
ImpaledTom <pokusik1@gmail.com>
2023-10-26 18:25:12 +0200

    dsad

--- pipi_upload/network_check.py
+++ pipi_upload/network_check.py
@@ -24,24 +24,20 @@ def network_check ():
             ip_eth0 = ni.ifaddresses ("eth0")[ni.AF_INET][0]["addr"]
             
         except Exception as ip_e_eth0:
-            print (ip_e_eth0)
+            #print (ip_e_eth0)
             ip_eth0 = 0
             
         try:
             ip_wlan0 = ni.ifaddresses ("wlan0")[ni.AF_INET][0]["addr"]
         
         except Exception as ip_e_wlan0:
-            print (ip_e_wlan0)
+            #print (ip_e_wlan0)
             ip_wlan0 = 0
-            
-        
-            
+                
             #print('My public IP address is: {}'.format(ip))    
             print('My local eth0 IP address is: {}'.format(ip_eth0))  
             print('My local wlan0 IP address is: {}'.format(ip_wlan0))  
-            
-       
-        
+      
         if ip_eth0 or ip_wlan0 != 0:
             
             if ip_eth0 !=0:
