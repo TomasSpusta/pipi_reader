2f263b6acbdab55190abe98a6765ae40f7b9bbac
ImpaledTom <pokusik1@gmail.com>
2023-11-27 13:59:47 +0100

    changes

--- pipi_upload/loading.py
+++ pipi_upload/loading.py
@@ -1,9 +1,9 @@
 import LCD_display
 from time import sleep
+import config
 
 
-LCD_display.display ("Loading packages.","Please wait.", "" ,"" ,clear=True, backlight_status=True)
-sleep (5)
+LCD_display.display ("Loading packages.","Please wait.", "" ,"" ,clear=True, backlight_status=True, sleep=5)
 
 from network_check import network_check
 
@@ -11,25 +11,23 @@ from network_check import network_check
 try:
     network_check ()
 except Exception as network_error:
+    LCD_display.display ("Network Error",str(network_error),"" ,"" ,True, True, 2) 
 
-    LCD_display.display (network_error,"","" ,"",clear=True, backlight_status=True) 
+if config.online_status == True :
 
-sleep (1)
+    from github_check import github_check
 
-from github_check import github_check
+    #Connect to GIT HUB and download the latest version from "main", "develop", "pipired" branch   
+    try:
+        github_check (branch = "develop")    
+    except Exception as github_error:
+        LCD_display.display ("Repository error", github_error,"" ,"",clear=True, backlight_status=True,sleep=2) 
 
-#Connect to GIT HUB and download the latest version from "main" or "develop" branch   
-try:
-    github_check (branch = "develop")    
-except Exception as github_error:
-    LCD_display.display (github_error,"","" ,"",clear=True, backlight_status=True) 
+    try:
+        LCD_display.display ("Loading program","","" ,"",clear=True, backlight_status=True, sleep=3)
+        import pipi_reader_main
+    except Exception as pipi_reader_main_Error:
+        LCD_display.display ("Main program error",pipi_reader_main_Error,"" ,"",clear=True, backlight_status=True)    
+        print (pipi_reader_main_Error)
+        
 
-LCD_display.display ("Loading program","","" ,"",clear=True, backlight_status=True)
-
-sleep (3)
-
-try:
-    import pipi_reader_main
-except Exception as pipi_reader_main_Error:
-    LCD_display.display (pipi_reader_main_Error,"","" ,"",clear=True, backlight_status=True)    
-    print (pipi_reader_main_Error)
