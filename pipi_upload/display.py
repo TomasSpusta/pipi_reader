7c5cc583f9f2f3d8f111e6855a7ca64bc886312b
ImpaledTom <pokusik1@gmail.com>
2024-01-09 12:00:31 +0100

    dfasdf

--- pipi_upload/display.py
+++ pipi_upload/display.py
@@ -1,6 +1,21 @@
 from LCD_display import display
+import netifaces as ni
+
+
+
 try:
-    display(        "Pokus",        " ",        " ",        " ", True, True, 2
+    ip_eth0 = ni.ifaddresses ("eth0")[ni.AF_INET][0]["addr"]         
+except Exception as ip_e_eth0:
+    print (ip_e_eth0)
+
+try:
+    ip_wlan0 = ni.ifaddresses ("wlan0")[ni.AF_INET][0]["addr"]       
+except Exception as ip_e_wlan0:
+    print (ip_e_wlan0)
+
+
+try:
+    display(ip_eth0,ip_e_eth0,ip_wlan0,ip_e_wlan0, True, True, 2
     )
 except Exception as e:
     display(        str(e),        " ",        " ",        " ", True, True, 2
