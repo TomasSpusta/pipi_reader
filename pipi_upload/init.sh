b3e775c9dff30c0fdf9320094f9413f47a689625
ImpaledTom <pokusik1@gmail.com>
2024-01-15 17:38:30 +0100

    fasdfasd

--- pipi_upload/init.sh
+++ pipi_upload/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "---GITHUB UPDATE---"
-/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/github_update.py
+/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/init_files/github_update.py
 
 echo "---GET MAC ADDRESS---"
-/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/get_mac_address.py
+/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/init_files/get_mac_address.py
 
 echo "---GET TOKEN---"
-/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/get_token.py
+/usr/bin/python3 /home/bluebox/pipi_reader/pipi_upload/init_files/get_token.py
