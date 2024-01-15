1fc02655c651f39ce0012dc53c9c96975c47758e
ImpaledTom <pokusik1@gmail.com>
2024-01-15 17:59:10 +0100

    fasdfa

--- pipi_upload/loading.py
+++ pipi_upload/loading.py
@@ -1,13 +1,22 @@
+
+from log import open_sh, write_log
+print ("marker1")
+
+
 from datetime import datetime
+print ("marker2")
 from lcd_display import display
+print ("marker3")
 
 import glob_vars
+print ("marker4")
 
 from network_check import connection_check
+print ("marker5")
 from pipi_reader_main import main
+print ("marker6")
 
-print ("marker1")
-from log import open_sh, write_log
+
 
 
 
@@ -26,7 +35,7 @@ if glob_vars.online_status == True :
     
     display ("Loading program","","" ,"",True,True, 2)
     
-    print ("marker2")
+    
     open_sh(glob_vars.mac_address)   
     write_log(1, datetime.now())
     write_log(2, glob_vars.ip_eth0, datetime.now())
