09ee65217756a1c5dfd29840750a56b57c1617ab
ImpaledTom <pokusik1@gmail.com>
2024-01-15 13:01:55 +0100

    fasdfa

--- pipi_upload/log_temp.py
+++ pipi_upload/log_temp.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 
 def write_log_temp (log_message):
-    #temp_log_address = "/home/bluebox/pipi_reader/pipi_upload/temp_log.txt"
-    temp_log_address = "pipi_upload/temp_log.txt"
+    temp_log_address = "/home/bluebox/pipi_reader/pipi_upload/log_temp.txt"
+    #temp_log_address = "pipi_upload/temp_log.txt"
     f = open (temp_log_address, "a")
     f.write (str(datetime.now()))
     f.write ("\n")
