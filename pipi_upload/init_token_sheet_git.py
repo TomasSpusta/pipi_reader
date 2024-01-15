2cc83848462a54a1ea864ff2bf6e0ce8cd58a1a9
ImpaledTom <pokusik1@gmail.com>
2024-01-15 16:01:04 +0100

    fasdf

--- pipi_upload/init_token_sheet_git.py
+++ pipi_upload/init_token_sheet_git.py
@@ -6,8 +6,9 @@ from github_check import git_update
 from network_check import get_mac_address
 from web_requests import get_token 
 from time import sleep
+from log import open_sh, write_log
+import glob_vars
 
-import os
 
 
 #config = ConfigParser()
@@ -18,6 +19,9 @@ get_mac_address ()
 sleep (1)
 get_token ()
 sleep (1)
+open_sh (glob_vars.mac_address)
+sleep(1)
+write_log (1,"pokus","pokus")
 
 
 
