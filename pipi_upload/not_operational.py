08006f8767380f4321ff30acb5f1544567546fcc
ImpaledTom <pokusik1@gmail.com>
2024-04-04 12:50:36 +0200

    sss

--- pipi_upload/not_operational.py
+++ pipi_upload/not_operational.py
@@ -1,2 +1,2 @@
 from lcd_display import display
-display ("Dear users", "Blueboxes are temporary","not operational.", "Working on it." ,clear=True, backlight_status=False) 
+display ("Dear users. Blueboxes", "are temporary","not operational.", "Working on it." ,clear=True, backlight_status=False) 
