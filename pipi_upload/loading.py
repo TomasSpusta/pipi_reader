f5eb8d878a3625c133c34bfd36d1712c5d7cfede
ImpaledTom <pokusik1@gmail.com>
2024-04-11 09:47:51 +0200

    sleep between logs

--- pipi_upload/loading.py
+++ pipi_upload/loading.py
@@ -1,6 +1,7 @@
 
 from log import open_sh, write_log
 from datetime import datetime
+from time import sleep
 from lcd_display import display
 import glob_vars
 from network_check import connection_check
@@ -22,13 +23,18 @@ if glob_vars.online_status == True :
     display ("Loading program","","" ,"",True,True, 2)
     
     
-    open_sh()   
+    open_sh()
+    sleep (0.2)   
     write_log(1, datetime.now())
+    sleep (0.2)   
     write_log(2, glob_vars.ip_eth0, datetime.now())
+    sleep (0.2)   
     write_log(3, glob_vars.ip_wlan0, datetime.now())
+    sleep (0.2)   
     write_log(5, glob_vars.equipment_name, datetime.now())
+    sleep (0.2)   
     write_log(14, "Branch: gpio-zero-app")
-    
+    sleep (0.2)   
     
     main ()
 
